e5d48318b31cd842e74ad4249a7444c931164cde
Afiabashir <64405026+Afiabashir@users.noreply.github.com>
2020-07-02 20:07:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+function getNumber(num) {
+    var num1 = document.getElementById("result");
+    num1.value += num;
+}
+
+
+function getClear() {
+    var num2 = document.getElementById("result");
+    num2.value = ""
+}
+
+
+function getResult() {
+    var num3 = document.getElementById("result");
+    result.value = eval(result.value)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
